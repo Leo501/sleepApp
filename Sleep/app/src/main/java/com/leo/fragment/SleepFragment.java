5a6816174b4c2155c8a8c613c050027a4ccee67d
leo <leo704373978@qq.com>
2016-07-01 00:18:50 +0800

    modify the fist fragment/sleepFragment add new layout & interface

--- Sleep/app/src/main/java/com/leo/fragment/SleepFragment.java
+++ Sleep/app/src/main/java/com/leo/fragment/SleepFragment.java
@@ -1,23 +1,50 @@
 package com.leo.fragment;
 
 
+import android.app.Application;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.leo.sleep.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Leo on 2016/5/14.
  */
-public class SleepFragment extends Fragment {
+public class SleepFragment extends Fragment implements RadioGroup.OnCheckedChangeListener{
+    private RadioGroup radioGroup;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_sleep,container,false);
+        radioGroup=(RadioGroup)view.findViewById(R.id.sleep_mood_rg);
+
+        radioGroup.setOnCheckedChangeListener(this);
         return view;
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.sleep_mood_rb_1:
+
+                Toast.makeText(getActivity().getApplicationContext(),"你选择了好心情",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sleep_mood_rb_2:
+                Toast.makeText(getActivity().getApplicationContext(),"你选择了坏心情",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sleep_mood_rb_3:
+                Toast.makeText(getActivity().getApplicationContext(),"你选择了没心情",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
